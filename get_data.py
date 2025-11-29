b0cf57a617b58b447bc2bf457702a516b3d8b90b
Tin Nguyen <91058119+t-i-n2003@users.noreply.github.com>
2025-11-29 15:25:52 +0700

    Initialize VN30 data processing in get_data.py
    Added imports and data processing for VN30 stock symbols.

--- get_data.py
+++ get_data.py
@@ -1,3 +1,25 @@
+import os
+from pathlib import Path
+
+vn_dir = Path.home() / ".vnstock"
+os.makedirs(vn_dir, exist_ok=True)
+
+from get_data import get_data
+from clean_data import clean_data
+from train_model import train_model
+import datetime as dt
+VN30 = [
+    "ACB", "BID", "CTG", "FPT", "GVR", "HDB", "HPG", "MBB", "MSN",
+    "MWG", "NVL", "PDR", "PLX", "POW", "SAB", "SSI", "STB", "TCB",
+    "TPB", "VCB", "VHM", "VIC", "VJC", "VNM", "VPB", "VRE", "GAS", "KDH", "PNJ", "REE"
+]
+for symbol in VN30:
+    start_date = '2015-01-01'
+    end_date = dt.date.today().strftime('%Y-%m-%d')
+    get_df = get_data(symbol, start_date, '2024-12-31')
+    clean_df = clean_data(get_df, symbol)
+    model = train_model(clean_df, symbol = symbol)
+    print(f"Model saved to {model}")    
 from vnstock import Vnstock
 import pandas as pd
 
